f79f55e199c669df0a0666fe5328d8291375a001
Pedroca <132005678+Pedro3DS@users.noreply.github.com>
2024-08-07 13:24:54 -0400

    Update

---
+++ portifolio/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container">
+      <div className="Header">
+        <div className="Nav">
+          <div className="Name">
+            <h1>Pedro Diniz</h1>
+
+          </div>
+          <div className="Links">
+            <p>Sobre mim</p>
+            <p>Técnologias</p>
+            <p>Projetos</p>
+            <p>Jogos</p>
+          </div>
+        </div>  
+      </div>
+      <div className="About">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;

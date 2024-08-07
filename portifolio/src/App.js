b222b8aa08d4bc42b97d3b45ef537253f9904334
Pedroca <132005678+Pedro3DS@users.noreply.github.com>
2024-08-07 19:40:19 -0400

    update

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,23 +1,15 @@
 import './css/App.css';
 import {React} from "react";
 import Picture from "./blank-profile-picture-973460_1280.webp";
+import Header from './components/Header/Header';
+import { FaGithub } from "react-icons/fa";
 
 function App() {
   return (
     <div className="Container">
+      <Header/>
       <div className="Container1">
-        <div className="Header">
-          <div className="Name">
-            <h1>Pedro Diniz</h1>
-          </div>
-          <div className="Links">
-            <p>Sobre mim</p>
-            <p>Técnologias</p>
-            <p>Projetos</p>
-            <p>Jogos</p>
-          </div>
-        </div>  
-        <div className="Contant">
+        <div className="Content">
           <div className="Infos">
 
             <h1>👋Olá, eu sou <span>Pedro Diniz Santana</span></h1>
@@ -25,6 +17,7 @@ function App() {
           </div>
           <div className="Picture" >
             <img src={Picture} alt='qwer' className='ProfilePicture'/>
+            <FaGithub/>
           </div>
         </div>
       </div>
--- portifolio/src/components/Header/Header.js
+++ portifolio/src/components/Header/Header.js
@@ -1,39 +1,21 @@
-import './css/App.css';
-import './'
+import '../../css/App.css';
+import './header.css'
 import {React} from "react";
-import Picture from "./blank-profile-picture-973460_1280.webp";
 
-function App() {
+function Header() {
   return (
-    <div className="Container">
-      <div className="Container1">
-        <div className="Header">
-          <div className="Name">
-            <h1>Pedro Diniz</h1>
-          </div>
-          <div className="Links">
-            <p>Sobre mim</p>
-            <p>Técnologias</p>
-            <p>Projetos</p>
-            <p>Jogos</p>
-          </div>
-        </div>  
-        <div className="Contant">
-          <div className="Infos">
-
-            <h1>👋Olá, eu sou <span>Pedro Diniz Santana</span></h1>
-            <p>Desenvolvedor Web Full-Stack & Desenvolvedor de Jogos Digitais</p>
-          </div>
-          <div className="Picture" >
-            <img src={Picture} alt='qwer' className='ProfilePicture'/>
-          </div>
+    <div className="Header">
+        <div className="Name">
+        <h1>Pedro Diniz</h1>
         </div>
-      </div>
-      <div className="About">
-        
-      </div>
-    </div>
+        <div className="Links">
+        <p>Sobre mim</p>
+        <p>Técnologias</p>
+        <p>Projetos</p>
+        <p>Jogos</p>
+        </div>
+    </div>  
   );
 }
 
-export default App;
+export default Header;

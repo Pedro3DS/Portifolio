070f0b56bb2f550489ecc72691c3a200ff98c758
Mauriiicio <132005678+Pedro3DS@users.noreply.github.com>
2025-09-26 17:30:17 -0400

    Update

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -5,6 +5,8 @@ import SplitText from "@/components/SplitText";
 import Balatro from "@/components/Balatro";
 import LoadingScreen from "@/components/LoadingScreen";
 import ElectricBorder from "@/blocks/Animations/ElectricBorder/ElectricBorder";
+import StarBorder from "@/blocks/Animations/StarBorder/StarBorder";
+import PillNav from "@/blocks/Components/PillNav/PillNav";
 
 export default function Home() {
 
@@ -20,9 +22,28 @@ export default function Home() {
           color1="green"
         />
       </div>
-      <div className="relative z-1 font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="relative z-1 font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen ">
+        <header className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <PillNav
+            logo="none"
+            logoAlt="Company Logo"
+            items={[
+              { label: 'Home', href: '/' },
+              { label: 'About', href: '/about' },
+              { label: 'Services', href: '/services' },
+              { label: 'Contact', href: '/contact' }
+            ]}
+            activeHref="/"
+            className="custom-nav"
+            ease="power2.easeOut"
+            baseColor="#000000"
+            pillColor="#ffffff"
+            hoveredPillTextColor="#ffffff"
+            pillTextColor="#000000"
+          /></header>
+        <main ></main>
+        {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <SplitText
             text="Hello, GSAP!"
             tag="h1"
@@ -36,23 +57,6 @@ export default function Home() {
             priority
           />
 
-
-          <ElectricBorder
-            color="#7df9ff"
-            speed={1}
-            chaos={0.5}
-            thickness={2}
-            style={{ borderRadius: 16 }}
-          >
-
-            <Image
-              src="/MyImages/20250918_201048.jpg"
-              alt="Next.js logo"
-              width={180}
-              height={38}
-              priority
-            />
-          </ElectricBorder>
           <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
             <li className="mb-2 tracking-[-.01em]">
               Get started by editing{" "}
@@ -91,54 +95,8 @@ export default function Home() {
               Read our docs
             </a>
           </div>
-        </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
+        </main> */}
+
       </div>
     </div>
   );
